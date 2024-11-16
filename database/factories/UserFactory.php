e706a065c1af4cc14799c0c75f1ab0a21858c6dd
ricomuh <rico.smartlolo@gmail.com>
2024-11-16 18:16:50 +0700

    Update UserFactory to use underscore in username slugs

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,7 +31,7 @@ class UserFactory extends Factory
         return [
             // 'fullname' => fake()->name(),
             'fullname' => $name,
-            'username' => str($name)->lower()->slug(),
+            'username' => str($name)->lower()->slug('_'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
