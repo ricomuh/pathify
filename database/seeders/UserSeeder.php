eeb089112c033390be699e198ec9c13fe85e12c6
ricomuh <rico.smartlolo@gmail.com>
2024-11-16 16:46:04 +0700

    Add UserSeeder to create users with roles; remove test user from DatabaseSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RoleEnum;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // make admin
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'role_id' => RoleEnum::Admin,
+        ]);
+
+        // make mentor
+        User::factory()->create([
+            'name' => 'Mentor',
+            'email' => 'mentor@example.com',
+            'role_id' => RoleEnum::Mentor,
+        ]);
+
+        // make user
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'role_id' => RoleEnum::User,
+        ]);
+    }
+}
