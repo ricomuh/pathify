b496fc9f87cc9fe5a336564ab36bee82f74bf2a5
ricomuh <rico.smartlolo@gmail.com>
2024-11-17 00:44:24 +0700

    feat: add relationships to CourseComment model for user, course, courseContent, and parent comments

--- app/Models/CourseComment.php
+++ app/Models/CourseComment.php
@@ -11,4 +11,24 @@ class CourseComment extends Model
     use HasFactory;
 
     protected $fillable = ['course_id', 'user_id', 'course_content_id', 'parent_id', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function courseContent()
+    {
+        return $this->belongsTo(CourseContent::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(CourseComment::class, 'parent_id');
+    }
 }
