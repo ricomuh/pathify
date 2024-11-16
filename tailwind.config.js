5e3a07c6de356d50a3570b814a525f3ca97fdab7
khafidhasukma <khafidhasukma@gmail.com>
2024-11-16 17:39:22 +0700

    feat: add custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,49 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
+        colors: {
+            primary: {
+                DEFAULT: "#4696E6",
+                surface: "#DAEAFA",
+                border: "#C1DCF7",
+                hover: "#3A7DC0",
+                pressed: "#234B73",
+                focus: "#DAEAFA",
+            },
+            secondary: {
+                DEFAULT: "#50D7BE",
+                surface: "#DCF7F2",
+                border: "#C5F2E9",
+                hover: "#43B39E",
+                pressed: "#286B5F",
+                focus: "#DCF7F2",
+            },
+            neutral: {
+                10: "#FDFDFD",
+                20: "#F5F5F6",
+                30: "#EEEEEE",
+                40: "#E2E2E3",
+                50: "#C5C6C6",
+                60: "#A3A4A5",
+                70: "#7C7E7F",
+                80: "#696B6C",
+                90: "#494C4D",
+                100: "#171A1B",
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                gabarito: ["Gabarito", "sans-serif"],
             },
         },
     },
