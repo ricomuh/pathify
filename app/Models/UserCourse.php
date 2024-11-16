0d88858dfc9e608d65b3c7282f08a17222a42bd5
ricomuh <rico.smartlolo@gmail.com>
2024-11-17 00:56:53 +0700

    feat: create UserCourse model and migration; implement user-course relationships and progress tracking

---
+++ app/Models/UserCourse.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCourse extends Model
+{
+    protected $fillable = ['user_id', 'course_id', 'is_favorite', 'progress', 'last_watched_at', 'completed_at'];
+
+    protected function casts()
+    {
+        return [
+            'is_favorite' => 'boolean',
+            'progress' => 'integer',
+            'last_watched_at' => 'datetime',
+            'completed_at' => 'datetime',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function getProggresPercentageAttribute()
+    {
+        // get total episode
+        $totalEpisode = $this->course->contents->count();
+
+        // get progress
+        $progress = $this->progress;
+
+        // calculate percentage
+        $percentage = ($progress / $totalEpisode) * 100;
+
+        return $percentage;
+    }
+
+    public function getCompletedAttribute()
+    {
+        return $this->completed_at ? true : false;
+    }
+}
---
+++ database/migrations/2024_11_16_175135_create_user_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Course;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Course::class, 'course_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_favorite')->default(false);
+            // progress in episode
+            $table->integer('progress')->default(0);
+            $table->timestamp('last_watched_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_courses');
+    }
+};
