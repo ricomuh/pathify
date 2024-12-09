a0f5a2a47a583efdc22db8f73846c37e158832a2
ricomuh <rico.smartlolo@gmail.com>
2024-12-09 19:24:04 +0700

    feat: add related courses retrieval based on questionnaire results in MyCourseController

--- app/Http/Controllers/User/MyCourseController.php
+++ app/Http/Controllers/User/MyCourseController.php
@@ -37,10 +37,45 @@ class MyCourseController extends Controller
             ->whereIn('id', $userCourses->pluck('course_id'))
             ->get();
 
+        // get related courses based on the questionnaire result and user courses
+        // sometimes the second category is null
+        // get the categories first
+        $categories = [
+            $questionnaireResult->firstCategory->id,
+        ];
+
+        if ($questionnaireResult->secondCategory) {
+            $categories[] = $questionnaireResult->secondCategory->id;
+        }
+
+        // append the user courses (if not exists)
+        $coursesCategories = $courses->map(function ($course) {
+            return $course->categories->pluck('id');
+        })->flatten()->unique();
+
+        $categories = array_merge($categories, $coursesCategories->toArray());
+
+        $relatedCourses = Course::with([
+            'mentor' => function ($query) {
+                $query->select('id', 'fullname', 'username', 'profile_picture');
+                $query->with('mentorDetail');
+            },
+            'categories',
+            'joined'
+        ])
+            ->whereHas('categories', function ($query) use ($categories) {
+                $query->whereIn('id', $categories);
+            })
+            ->whereNotIn('id', $userCourses->pluck('course_id'))
+            ->inRandomOrder()
+            ->limit(6)
+            ->get();
+
         // return response()->json(compact('courses'));
         return response()->json([
             'courses' => $courses,
             'questionnaire_result' => $questionnaireResult,
+            'related_courses' => $relatedCourses,
         ]);
     }
 }
