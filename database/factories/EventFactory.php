94806ba7c21bf6f5c2acb34c8cd73323b3f21d79
ricomuh <rico.smartlolo@gmail.com>
2024-12-12 23:22:16 +0700

    feat: update EventFactory to adjust event and registration date generation logic

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,7 +16,10 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
-        $start = $this->faker->dateTimeBetween('-1 month', '+1 month');
+        // $start = $this->faker->dateTimeBetween('-1 month', '+1 month');
+        $regisStart = $this->faker->dateTimeBetween('-1 month', '+1 month');
+        $regisEnd = $this->faker->dateTimeBetween($regisStart, '+1 month');
+        $start = $this->faker->dateTimeBetween($regisEnd, '+1 month');
         /**
          * rundown:
          * [
@@ -43,7 +46,7 @@ class EventFactory extends Factory
             $platform = $this->faker->randomElement(['Zoom', 'Google Meet', 'Microsoft Teams']);
             $link = $this->faker->url;
         } else {
-            $location = $this->faker->address;
+            // $location = $this->faker->address;
             $locationName = $this->faker->company;
             $locationAddress = $this->faker->address;
         }
@@ -62,8 +65,9 @@ class EventFactory extends Factory
             'title' => $this->faker->sentence(),
             'body' => $this->faker->paragraphs(3, true),
             'start_date' => $start,
+            'end_date' => $start->modify('+3 hours'),
             // add 1 to 3 hours to start date
-            'end_date' => $this->faker->dateTimeBetween($start, $start->modify('+3 hours')),
+            // 'end_date' => $this->faker->dateTimeBetween($start, $start->modify('+3 hours')),
             'quota' => $this->faker->numberBetween(100, 150),
             // 'location' => $this->faker->address,
             'is_online' => $is_online,
@@ -71,8 +75,10 @@ class EventFactory extends Factory
             'link' => $link ?? null,
             'location_name' => $locationName ?? null,
             'location_address' => $locationAddress ?? null,
-            'registration_start_date' => $this->faker->dateTimeBetween('-1 month', $start),
-            'registration_end_date' => $this->faker->dateTimeBetween($start, '+1 month'),
+            // 'registration_start_date' => $this->faker->dateTimeBetween('-1 month', $start),
+            // 'registration_end_date' => $this->faker->dateTimeBetween($start, '+1 month'),
+            'registration_start_date' => $regisStart,
+            'registration_end_date' => $regisEnd,
             'status' => $this->faker->randomElement(['draft', 'published']),
             'rundown' => $rundown,
             'faqs' => $faqs,
